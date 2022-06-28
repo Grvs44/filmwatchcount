0bbed4ead86905a889748d731b78ca2216f8bc2d
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2022-06-28 16:03:34 +0100

    Commit of changes before move to Django Forms

--- filmwatch/tracker/urls.py
+++ filmwatch/tracker/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', HomeView.as_view()),
-    path('<str:table>', FilmList.as_view())
+    path('<str:table>', FilmList.as_view()),
+    path('<str:table>/remove', RemoveFilm.as_view()),
+    path('<str:table>/update', UpdateFilm.as_view())
 ]
--- filmwatch/tracker/views.py
+++ filmwatch/tracker/views.py
@@ -1,8 +1,7 @@
-from http.client import HTTPResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse, Http404, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest
 from .models import *
 import json
 from datetime import datetime
@@ -14,7 +13,7 @@ class FilmList(LoginRequiredMixin,View):
         if table == "watch": table = FilmWatch
         elif table == "film": table = Film
         elif table == "group": table = FilmGroup
-        else: raise Http404
+        else: return HttpResponseNotFound()
         items = table.objects.filter(User=request.user)
         itemlist = []
         for item in items:
@@ -23,14 +22,59 @@ class FilmList(LoginRequiredMixin,View):
     def post(self,request,table):
         try:
             if table == "watch":
-                FilmWatch(User=request.user, Film_id=int(request.POST["select"]), DateWatched=datetime.strptime(request.POST["date"], "%Y-%m-%d").date(), Notes=request.POST["notes"]).save()
+                if request.POST["select"] == "": filmid = None
+                else: filmid = int(request.POST["select"])
+                FilmWatch(User=request.user, Film_id=filmid, DateWatched=datetime.strptime(request.POST["date"], "%Y-%m-%d").date(), Notes=request.POST["notes"]).save()
             elif table == "film":
-                groupid = int(request.POST["select"])
-                if groupid == -1: groupid = None
+                if request.POST["select"] == "": groupid = None
+                else: groupid = int(request.POST["select"])
                 Film(User=request.user, FilmGroup_id=groupid, Name=request.POST["film"]).save()
             elif table == "filmgroup":
                 FilmGroup(User=request.user, Name=request.POST["name"]).save()
             else:
-                raise Http404
+                return HttpResponseNotFound()
             return HttpResponse()
-        except (ValueError,IndexError,TypeError): raise HttpResponseBadRequest()
+        except (ValueError,IndexError,TypeError): return HttpResponseBadRequest()
+class RemoveFilm(FilmList):
+    def post(self,request,table):
+        try:
+            recordid = int(request.POST["id"])
+            if table == "watch":
+                get_object_or_404(FilmWatch,pk=recordid).delete()
+            elif table == "film":
+                get_object_or_404(Film,pk=recordid).delete()
+            elif table == "filmgroup":
+                get_object_or_404(FilmGroup,pk=recordid).delete()
+            else:
+                return HttpResponseNotFound()
+            if object.User == request.user:
+                object.delete()
+                return HttpResponse()
+            else:
+                return HttpResponseForbidden("The requested item cannot be deleted by this user",content_type="text/plain")
+        except (ValueError,IndexError,TypeError): return HttpResponseBadRequest()
+class UpdateFilm(FilmList):
+    def post(self,request,table,id):
+        try:
+            if table == "watch":
+                object = get_object_or_404(FilmWatch,pk=id)
+                if request.POST["select"] == "": object.Film_id = None
+                else: object.Film_id = int(request.POST["select"])
+                object.DateWatched = datetime.strptime(request.POST["date"], "%Y-%m-%d").date()
+                object.Notes = request.POST["notes"]
+            elif table == "film":
+                object = get_object_or_404(Film,pk=id)
+                if request.POST["select"] == "": object.FilmGroup_id = None
+                else: object.FilmGroup_id = int(request.POST["select"])
+                object.Name = request.POST["name"]
+            elif table == "filmgroup":
+                object = get_object_or_404(FilmGroup,pk=id)
+                object.Name = request.POST["name"]
+            else:
+                return HttpResponseNotFound()
+            if object.User == request.user:
+                object.save()
+                return HttpResponse()
+            else:
+                return HttpResponseForbidden("The requested item cannot be modified by this user",content_type="text/plain")
+        except (ValueError,IndexError,TypeError,KeyError): raise HttpResponseBadRequest()
