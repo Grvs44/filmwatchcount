d2f6c0c6edc57113c8f9946c02782630be0cfc3b
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-05-23 09:46:33 +0100

    Add verbose_name

--- filmwatchcount/apps.py
+++ filmwatchcount/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class FilmwatchcountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filmwatchcount'
+    verbose_name = 'Film Watch Count'
