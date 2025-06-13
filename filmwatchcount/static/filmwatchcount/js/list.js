dcef3a4156b5a111e0ddde23dae5ab4c1fe3d7eb
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-06-13 12:29:41 +0100

    Check sort radio button

--- filmwatchcount/static/filmwatchcount/js/list.js
+++ filmwatchcount/static/filmwatchcount/js/list.js
@@ -1,5 +1,9 @@
 ;(()=>{
   let p = new URLSearchParams(location.search)
   if(p.has('q'))document.getElementsByName('q')[0].value = p.get('q')
+  if(p.has('sort')){
+    const v = p.get('sort')
+    document.getElementsByName('sort').forEach(e=>e.checked=e.value==v)
+  }
 })()
 const clear=()=>document.getElementsByName('sort').forEach(e=>e.checked=false)
