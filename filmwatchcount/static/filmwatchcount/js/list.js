5027252fa41ebd0010ecfcb0d1c9939745c24cb8
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-06-13 12:09:56 +0100

    Change page number position

--- filmwatchcount/static/filmwatchcount/js/list.js
+++ filmwatchcount/static/filmwatchcount/js/list.js
@@ -1,4 +1,3 @@
-document.body.appendChild(document.getElementById('page').cloneNode(true))
 ;(()=>{
   let p = new URLSearchParams(location.search)
   if(p.has('q'))document.getElementsByName('q')[0].value = p.get('q')
